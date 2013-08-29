083870d27a0c8ba8c4484707a7b343bc57bfb354
shahid930 <shahidiqbal930@gmail.com>
2013-08-29 22:59:47 +0500

    j

--- WindowsFormsApplication4/Form1.cs
+++ WindowsFormsApplication4/Form1.cs
@@ -15,5 +15,10 @@ namespace WindowsFormsApplication4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
